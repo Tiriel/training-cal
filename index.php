ce3886aa57251f302489736fc17f336c2137867a
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-06-24 17:07:15 +0200

    Add composer

--- index.php
+++ index.php
@@ -1,40 +1,40 @@
 <?php
 
+require_once __DIR__.'/vendor/autoload.php';
+
 use App\Auth\Exception\AuthException;
 use App\User\Admin;
 use App\User\Enum\AdminLevel;
 use App\User\Member;
-
-require_once __DIR__.'/src/Auth/Exception/AuthException.php';
-require_once __DIR__.'/src/Tools/ToStringTrait.php';
-require_once __DIR__.'/src/Auth/Interface/AuthInterface.php';
-require_once __DIR__ .'/src/User/AbstractUser.php';
-require_once __DIR__.'/src/User/Member.php';
-require_once __DIR__.'/src/User/Enum/AdminLevel.php';
-require_once __DIR__.'/src/User/Admin.php';
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 $m1 = new Member('User Ben', 'Ben', 'abcd1234', 37);
 $m2 = new Member('User Tom', 'Tom', 'abcd1234', 37);
 $a1 = new Admin('User Admin', 'Admin', 'admin1234', 35, AdminLevel::SuperAdmin);
 
-echo 'Member : '.Member::count()."\n"; // 2
-echo 'Admin : '.Admin::count()."\n"; // 1
+//echo 'Member : '.Member::count()."\n"; // 2
+//echo 'Admin : '.Admin::count()."\n"; // 1
 
-unset($m2);
+//unset($m2);
 
-echo 'Member : '.Member::count()."\n"; // 1
-echo 'Admin : '.Admin::count()."\n"; // 1
+//echo 'Member : '.Member::count()."\n"; // 1
+//echo 'Admin : '.Admin::count()."\n"; // 1
 
 try {
-    echo $m1->auth('Ben', 'abc1234')
-        ? "Authentified as {$m1->getName()}\n"
-        : "Wrong credentials\n";
+    $loader = new FilesystemLoader(['templates']);
+    $twig = new Environment($loader);
+
+    echo $twig->render('index.html.twig', [
+        'login' => $m1->getName(),
+        'auth' => $m1->auth('Ben', 'abcd1234'),
+    ]);
 } catch (AuthException $e) {
     echo $e->getMessage()."\n";
 }
 
-echo $a1->auth('Ben', 'abcd1234')
-    ? "Authentified as {$a1->getName()}\n"
-    : "Wrong credentials\n";
+//echo $a1->auth('Ben', 'abcd1234')
+//    ? "Authentified as {$a1->getName()}\n"
+//    : "Wrong credentials\n";
 
-echo $m1."\n";
+//echo $m1."\n";
