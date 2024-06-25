db8a394068006b33fe382372ce5beeff06debf40
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-06-25 12:26:34 +0200

    BookController

---
+++ src/Controller/BookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/book')]
+class BookController extends AbstractController
+{
+    #[Route('', name: 'app_book_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController::index',
+        ]);
+    }
+
+    #[Route('/{id<\d+>?0}', name: 'app_book_show', methods: ['GET'])]
+    //#[Route('/{id}', name: 'app_book_show', requirements: ['id' => '\d+'], defaults: ['id' => 0], methods: ['GET'])]
+    public function show(int $id = 1): Response
+    {
+        return $this->render('book/show.html.twig', [
+            'controller_name' => 'BookController::show - Id '.$id,
+        ]);
+    }
+}
